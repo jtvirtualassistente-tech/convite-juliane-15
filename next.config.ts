9c9152455373a0cafafe4b199009db5c05880f71
Julli <jt.virtual.assistente@gmail.com>
2026-07-16 20:06:54 -0300

    Evita cache no painel admin

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,28 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/admin",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+          },
+        ],
+      },
+      {
+        source: "/admin/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
